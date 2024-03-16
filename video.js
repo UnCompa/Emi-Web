b15233e12ac97045fdcdda6aaa8d23a70e2f1f22
UnCompa <125362398+UnCompa@users.noreply.github.com>
2024-03-16 09:45:22 -0500

    Add files via upload
    Signed-off-by: UnCompa <125362398+UnCompa@users.noreply.github.com>

---
+++ video.js
@@ -0,0 +1,11 @@
+const audio = new Plyr('audio', {
+  accentColor: "#0af"
+});
+const player = new Plyr('video', {
+  controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'fullscreen'],
+  accentColor: '#ff0000'
+});
+const youtube = new Plyr('#player', {
+  controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'fullscreen'],
+  accentColor: '#ff0000'
+});
